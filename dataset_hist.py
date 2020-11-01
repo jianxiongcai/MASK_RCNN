985f5b2c7070fc90a4fb0a19b7db8b1f1dba90b0
Jianxiong Cai <jicai@seas.upenn.edu>
2020-11-01 14:07:07 -0500

    [PATCH] dataset hist: plt.hist can not accept list of tensor input

--- dataset_hist.py
+++ dataset_hist.py
@@ -15,20 +15,17 @@ import matplotlib.patches as patches
 import matplotlib.mlab as mlab
 import os.path
 from dataset import BuildDataset,BuildDataLoader
+from tqdm import tqdm
 
 
 
 ## Visualize debugging
 if __name__ == '__main__':
     # file path and make a list
-#     todo (jianxiong): change this back before submitting
     imgs_path = 'data/hw3_mycocodata_img_comp_zlib.h5'
     masks_path = 'data/hw3_mycocodata_mask_comp_zlib.h5'
     labels_path = 'data/hw3_mycocodata_labels_comp_zlib.npy'
     bboxes_path = 'data/hw3_mycocodata_bboxes_comp_zlib.npy'
-
-   
-#    os.makedirs("testfig", exist_ok=True)
 
     paths = [imgs_path, masks_path, labels_path, bboxes_path]
     # load the data into data.Dataset
@@ -53,37 +50,43 @@ if __name__ == '__main__':
     test_build_loader = BuildDataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=0)
     test_loader = test_build_loader.loader()
 
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cpu")
     dataset_scale_list = []
     dataset_ratio_list = []
-    for idx, batch in enumerate(train_loader, 0):
+    for idx, batch in enumerate(tqdm(train_loader), 0):
         boxes = batch['bbox']
                   
         batch_size=len(boxes)
         for i in range(batch_size):
-            w=boxes[i][:,2].clone().detach().float()
-            h=boxes[i][:,3].clone().detach().float()
-            ratio=w/h
-            scale=torch.sqrt(w*h)
-            dataset_ratio_list+=ratio
-            dataset_scale_list+=scale
-            
-    def median(lst):
-        n = len(lst)
-        s = sorted(lst)
-        return (sum(s[n//2-1:n//2+1])/2.0, s[n//2])[n % 2] if n else None
+            for obj_i in range(boxes[i].shape[0]):
+                w=boxes[i][obj_i, 2].clone().detach().float()
+                h=boxes[i][obj_i, 3].clone().detach().float()
+                ratio=w/h
+                scale=torch.sqrt(w*h)
+                dataset_ratio_list.append(ratio.item())
+                dataset_scale_list.append(scale.item())
+
+    # todo (jianxiong): why not use np median
+    # def median(lst):
+    #     n = len(lst)
+    #     s = sorted(lst)
+    #     return (sum(s[n//2-1:n//2+1])/2.0, s[n//2])[n % 2] if n else None
     num_bins = 50
-    
-    fig1 = plt.figure()
-    n, bins, patch = plt.hist(dataset_scale_list, num_bins, facecolor='blue', edgecolor='black')
-    plt.title("Scale histogram with median = {}".format(median(dataset_scale_list)))
+
+    dataset_scales = np.array(dataset_scale_list)
+    dataset_ratios = np.array(dataset_ratio_list)
+
+    plt.figure()
+    n, bins, patch = plt.hist(dataset_scale_list, bins=num_bins, facecolor='blue', edgecolor='black')
+    plt.title("Scale histogram with median = {}".format(np.median(dataset_scales)))
     plt.xlabel('Scale')
     plt.ylabel('Occurrences')
     plt.savefig("scale.png")
     
-    fig2 = plt.figure()
-    n, bins, patch = plt.hist(dataset_ratio_list, num_bins, facecolor='blue', edgecolor='black')
-    plt.title("Aspect Ratio histogram with median = {}".format(median(dataset_ratio_list)))
+    plt.figure()
+    n, bins, patch = plt.hist(dataset_ratio_list, bins=num_bins, facecolor='blue', edgecolor='black')
+    plt.title("Aspect Ratio histogram with median = {}".format(np.median(dataset_ratios)))
     plt.xlabel('Aspect Ratio')
     plt.ylabel('Occurrences')
     plt.savefig("ratio.png")
